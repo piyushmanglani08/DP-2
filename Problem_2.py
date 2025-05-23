aaadd50f84cee7430a3b40b14d8f29a9a79f98fc
Piyush Manglani <piyushmanglani@Piyushs-MBP.lan>
2025-05-23 15:52:55 -0500

    complete

--- Problem_2.py
+++ Problem_2.py
@@ -1,17 +1,15 @@
 # Time Complexity : O(Coins * Amount)
 # Space Complexity : O(Coins * Amount)
 # Did this code successfully run on Leetcode : yes	
-# Any problem you faced while coding this : Followed approach from the class 
+
+
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        n = len(coins)
-   
-        m = [0] * (amount + 1)
-        a=amount
-        m[0]=1
-        for j in coins:
-            for i in range(1,a+1):
-                if i>=j:
-                    m[i]+=m[i-j]
-        return m[a]
-                
+        dp = [0] * (amount + 1)
+        dp[0] = 1  # Base case: 1 way to make amount 0
+
+        for coin in coins:
+            for j in range(coin, amount + 1):
+                dp[j] += dp[j - coin]
+
+        return dp[amount]
